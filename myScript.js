c7f081c71efa336fc34925ca121da4d845843bcd
JensGre <Jens.Grewe@gmx.net>
2016-12-23 20:37:53 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	
+	$( ".A1").change(function(){ 
+
+		if ($("#1").val().toLowerCase() === "k" &&
+			$("#2").val().toLowerCase() === "e" &&
+			$("#3").val().toLowerCase() === "r" &&
+			$("#4").val().toLowerCase() === "z" &&
+			$("#5").val().toLowerCase() === "e" &&
+			){
+			alert("Super! Nur blöd, dass hier keiner eine Kerze hat :)")
+		}
+	 });
+
+});
